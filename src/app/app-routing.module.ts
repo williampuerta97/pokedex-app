947c4686b3b6708e721dba58abf0081371110e1a
William Puerta <williampuerta1097@gmail.com>
2021-05-16 00:19:48 -0500

    Pokedex - Angular application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PokeTableComponent},
+  {path: 'poke-detail/:id', component: PokeDetailComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
